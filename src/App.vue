1482a62598ab0d1712dd6b4a337ffaacf13dd5af
TallTall22 <syuns1205@gmail.com>
2023-07-26 10:05:01 +0800

    debug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
   import { RouterLink, RouterView } from 'vue-router'
   import Header from './components/Header.vue'
-  import Footer from './components/Footer.Vue'
+  import Footer from './components/Footer.vue'
 </script>
 
 <template>
