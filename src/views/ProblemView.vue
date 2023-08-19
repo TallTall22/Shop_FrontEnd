8a043c6f8e068aab7f933657c35b5919719e583f
TallTall22 <syuns1205@gmail.com>
2023-08-20 05:03:01 +0800

    debug

--- src/views/ProblemView.vue
+++ src/views/ProblemView.vue
@@ -4,7 +4,7 @@ import { useRouter } from 'vue-router';
 import { io } from 'socket.io-client';
 import { useUserStore } from '../stores/user';
 
-const socket = io("wss://test.just-for-test-shop.de/", { transports: ['websocket'] });
+const socket = io("https://test.just-for-test-shop.de/", { transports: ['websocket'] });
 const userStore = useUserStore();
 const authToken = localStorage.getItem('authToken')
 const router = useRouter();
@@ -153,10 +153,6 @@ h2{
     }
     }
     }
-    
-
-
-
 }
 
 </style>
